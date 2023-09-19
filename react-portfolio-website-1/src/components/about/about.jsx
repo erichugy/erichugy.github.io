472a66d3fb713ec412be25e5e7720e7ef4f11926
Eric Huang <eric.huang3143@gmail.com>
2023-09-19 01:15:55 -0400

    Experience

--- react-portfolio-website-1/src/components/about/about.jsx
+++ react-portfolio-website-1/src/components/about/about.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {BiLibrary} from 'react-icons/bi'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1+ Year</small>
+            </article>
+            <article className='about__card'>
+              <BiLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>3 and counting</small>
+            </article>
+            {/* To do later */}
+            {/* <article className='volunteering__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Volunteering Work</h5>
+              <small>Read about my adventures!</small>
+            </article>
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Fun Times</h5>
+              <small>Read about my adventures!</small>
+            </article> */}
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus optio possimus facere? Quia laudantium sapiente, repudiandae accusamus doloribus id impedit explicabo adipisci illo mollitia ad autem ab est modi corrupti.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Chat</a>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
