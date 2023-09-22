af80cf582cd06c43fedb1c6ee9374414608564f7
Eric Huang <eric.huang3143@gmail.com>
2023-09-22 18:33:28 -0400

    Fixed some css for small devices

--- react-portfolio-website-1/src/components/portfolio/Portfolio.jsx
+++ react-portfolio-website-1/src/components/portfolio/Portfolio.jsx
@@ -4,8 +4,23 @@ import './portfolio.css'
 
 const Portfolio = () => {
 return (
-    <section id='portfolio'>Portfolio</section>
-)
-}
+    <section id='portfolio'>
+        <h5>My Recent Work</h5>
+        <h2>Portfolio</h2>
+
+
+        <div className="container portfolio__container">
+            <article className="portfolio__item">
+                <div className="portfolio__item-item">
+                    
+                </div>    
+                <h3>This is a pportfolio item</h3>
+                <a href="http://github.com" className='btn'>GitHub</a>
+                <a href="http://github.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+
+            </article>    
+        </div>
+    </section>
+)}
 
 export default Portfolio
