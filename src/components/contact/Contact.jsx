90ebe31cde3f27bdeaa8a4dd4ae7160346d55a89
Eric Huang <eric.huang3143@gmail.com>
2023-10-01 15:18:49 -0400

    Added message

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,7 +4,7 @@
 // This source code is licensed under the BSD-style license found in the
 // LICENSE file in the root directory of this source tree. 
 
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import "./contact.css";
 import { MdOutlineEmail } from "react-icons/md";
 import { RiMessengerFill } from "react-icons/ri"
@@ -15,17 +15,25 @@ import constants from "../../utils/constants.json"
 
 const Contact = () => {
   const form = useRef();
+  const [messageSent, setMessageSent] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm("service_cwhk9br","template_wrxusn6", form.current, 'gfSCS86EfsMfwLl-z')
+    emailjs.sendForm("service_cwhk9br", "template_wrxusn6", form.current, 'gfSCS86EfsMfwLl-z')
       .then((result) => {
+          setMessageSent(true);
+          setHasError(false);
           console.log(result.text);
       }, (error) => {
+          setHasError(true);
           console.log(error.text);
       });
+  };
 
-      e.target.reset() // change this to change submit button to submitted
+  const resetForm = () => {
+    setMessageSent(false);
   };
 
   return (
@@ -56,13 +64,21 @@ const Contact = () => {
             <a href="www.linkedin.com/in/erichugy">Send a message</a>
           </article> */}
         </div>
-          <form ref={form} onSubmit={sendEmail}>
-            <input type="text" name='firstname' placeholder='First Name' required/>
-            <input type="text" name='lastname' placeholder='Last Name' required/>
-            <input type="email" name='email' placeholder='Your Email' required />
-            <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
-            <button type='submit' className='btn btn-primary'>Send Message</button>
-          </form>
+        {
+          messageSent ? (
+            <div>
+              <button className='btn btn-primary' onClick={resetForm}>Send another message</button>
+            </div>
+          ) : (
+            <form ref={form} onSubmit={sendEmail}>
+              <input type="text" name='firstname' placeholder='First Name' required/>
+              <input type="text" name='lastname' placeholder='Last Name' required/>
+              <input type="email" name='email' placeholder='Your Email' required />
+              <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
+              <button type='submit' className='btn btn-primary'>{hasError ? "Retry" : "Send Message"}</button>
+            </form>
+          )
+        }
 
       </div>
     </section>
