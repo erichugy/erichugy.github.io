edc6818f48cccd3df6fc4950a5b9f23b2fadd2be
Eric Huang <eric.huang3143@gmail.com>
2024-02-28 13:45:19 +0800

    Starting to create hidden files

---
+++ src/components/hiddenDocuments/HiddenDocuments.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './hiddenDocuments.css'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header__container">
+      </div>
+    </header>
+  )
+}
+
+export default Header
